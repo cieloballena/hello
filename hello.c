1776ac7eb3eab0624e0cdee1e11eb5be4a8952bc
cieloballena <cieloballena@gmail.com>
2019-10-24 19:08:58 +0900

    final

--- hello.c
+++ hello.c
@@ -2,12 +2,8 @@
 #include "hello.h"
 
 int main(void){
-<<<<<<< HEAD
 	printf("Hello!!\n");
-=======
-	printf("Hello World\n");
 	printf("What's your name\n");
 	answer();
->>>>>>> test2
 	return 0;
 }
